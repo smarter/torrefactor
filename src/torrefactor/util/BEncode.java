527a428a82791a530a5277f53afada5510369644
Guillaume Martres <smarter@ubuntu.com>
2011-04-12 13:17:45 +0200

    BEncode: new function byte[] encode(BValue elem), other functions made private

--- src/torrefactor/util/BEncode.java
+++ src/torrefactor/util/BEncode.java
@@ -7,21 +7,21 @@ import java.io.*;
 import java.util.*;
 
 public class BEncode {
-    public static void encode(int val, ByteArrayOutputStream out)
+    private static void encode(int val, ByteArrayOutputStream out)
     throws IOException {
         out.write('i');
         out.write(Integer.toString(val).getBytes());
         out.write('e');
     }
 
-    public static void encode(byte[] string, ByteArrayOutputStream out)
+    private static void encode(byte[] string, ByteArrayOutputStream out)
     throws IOException {
         out.write(Integer.toString(string.length).getBytes());
         out.write(':');
         out.write(string);
     }
 
-    public static void encode(List<BValue> list, ByteArrayOutputStream out)
+    private static void encode(List<BValue> list, ByteArrayOutputStream out)
     throws IOException {
         out.write('l');
         for (BValue elem : list) {
@@ -30,7 +30,7 @@ public class BEncode {
         out.write('e');
     }
 
-    public static void encode(Map<String, BValue> map, ByteArrayOutputStream out)
+    private static void encode(Map<String, BValue> map, ByteArrayOutputStream out)
     throws IOException {
         out.write('d');
         for (Map.Entry<String, BValue> entry : map.entrySet()) {
@@ -40,7 +40,7 @@ public class BEncode {
         out.write('e');
     }
 
-    private static void encode(BValue elem, ByteArrayOutputStream out)
+    public static void encode(BValue elem, ByteArrayOutputStream out)
     throws IOException {
         Object object = elem.toObject();
         if (object instanceof Integer) {
@@ -52,5 +52,11 @@ public class BEncode {
         } else if (object instanceof Map) {
             encode(elem.toMap(), out);
         }
+    }
+
+    public static byte[] encode(BValue elem) {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        encode(elem, out);
+        return out.toByteArray();
     }
 }
