0590f0df82d611916f1ae70b7a4e9f2e1ef7c1a4
Florian Vessaz <florian.vessaz@epfl.ch>
2011-05-23 20:56:19 +0200

    Add javadoc to GenericArray

--- src/torrefactor/util/GenericArray.java
+++ src/torrefactor/util/GenericArray.java
@@ -1,18 +1,42 @@
 package torrefactor.util;
 
+/**
+ * A simple array wich allow to use safely generic types
+ */
 class GenericArray<T> {
-    public final Object[] array;
+    private final Object[] array;
+
+    /**
+     * The length of the array.
+     */
     public final int length;
 
+    /**
+     * Create a new array.
+     *
+     * @param size  the size of the array
+     */
     public GenericArray (int size) {
         this.array = new Object[size];
         this.length = this.array.length;
     }
 
+    /**
+     * Put an element in the array.
+     *
+     * @param index  the index where to but the object
+     * @param object the object to put in the array
+     */
     public void put (int index, T object) {
         this.array[index] = (Object) object;
     }
 
+    /**
+     * Get an element from the array.
+     *
+     * @param index the index of the object to return
+     * @return the element at the given position
+     */
     @SuppressWarnings(value = "unchecked")
     public T get (int index) {
         return (T) this.array[index];
