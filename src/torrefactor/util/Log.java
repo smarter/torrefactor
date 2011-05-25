c55769d7fe69e964e68d6a45010d25c43d757dfc
Florian Vessaz <florian.vessaz@epfl.ch>
2011-05-26 00:27:52 +0200

    Finish javadoc in Log

--- src/torrefactor/util/Log.java
+++ src/torrefactor/util/Log.java
@@ -10,6 +10,11 @@ import java.io.*;
 /**
  * This class manage a ring containing the log message. It can displays them as
  * they are added. It can also display the ring whenever you want.
+ *
+ * This class as some functionality that permits to enable/disable the logged
+ * message with flags (1 bit = 1 flag). But this functionality has finally not
+ * been used in the project and is therefore not well documented and not
+ * tested.
  */
 public class Log {
     private static Log instance;
@@ -28,7 +33,7 @@ public class Log {
     // indicate where the next element will be put
     private int ringPosition = 0;
 
-    public Log () {
+    private Log () {
         if (instance != null) {
             System.err.println ("Creating a new instance of Log while an "
                                 + "instance already exist. It's probably not "
@@ -41,11 +46,10 @@ public class Log {
         log (INFO, this, "Logging started at " + this.refTime);
     }
 
-    /* alias for get instance */
-    public static Log i () {
-        return getInstance ();
-    }
-
+    /**
+     * Return the instance of this class if it exists or create a new instance
+     * and return it.
+     */
     public static Log getInstance () {
         synchronized (Log.class) {
             if (instance == null) {
@@ -55,22 +59,37 @@ public class Log {
         return instance;
     }
 
+    /**
+     * Set the stream where the message will be printed.
+     */
     public void setStream (PrintStream stream) {
         this.printStream = stream;
     }
 
+    /**
+     * Set the stream where the message will be printed.
+     */
     public void setStream (OutputStream stream) {
         this.printStream = new PrintStream (stream);
     }
 
+    /**
+     * Set the mask which select enabled messages.
+     */
     public void setEnableMask (int mask) {
         this.enableMask = mask;
     }
     
+    /**
+     * Set the mask which disable message.
+     */
     public void setDisableMask (int mask) {
         this.disableMask = mask;
     }
 
+    /**
+     * Return the next unused bit in mask.
+     */
     public int getUnusedBitForMask () {
         int mask = 1;
         while (mask != 1 << 31) {
@@ -84,6 +103,13 @@ public class Log {
         return 0;
     }
 
+    /**
+     * Log a message.
+     *
+     * @param level     the mask of the message
+     * @param object    the header of the message
+     * @param message   the message to log
+     */
     public void log (int level, Object object, String message) {
         Quad<Integer, Long, Object, String> quad =
             new Quad<Integer, Long, Object, String> (level,
@@ -100,10 +126,16 @@ public class Log {
         if (this.printAdded) print (quad);
     }
 
+    /**
+     * Print the specified message.
+     */
     private void print (Quad<Integer, Long, Object, String> quad) {
         print (quad, this.enableMask, this.disableMask);
     }
 
+    /**
+     * Print the specified message.
+     */
     private void print (Quad<Integer, Long, Object, String> quad,
                         int enableMask, int disableMask) {
         if (quad == null) return;
@@ -124,16 +156,22 @@ public class Log {
                                 + " " + quad.fourth () + "\n");
     }
 
+    /**
+     * Print the ring of message.
+     */
     public void printRing () {
         printRing (this.enableMask, this.disableMask);
     }
 
-    /* Prints all message in the ring regardless of their flags. */
+    /**
+     * Prints all message in the ring regardless of their flags.
+     */
     public void printRingAll () {
         printRing (~0, 0);
     }
 
-    /* Prints all message in the ring matching the specified flags. */
+    /** Prints all message in the ring matching the specified flags.
+     */
     public void printRing (int enableMask, int disableMask) {
         int current = this.ringPosition;
         int end = current - 1;
@@ -148,8 +186,10 @@ public class Log {
         print (this.ring.get (current));
     }
 
-    // Print at most the last num elements from the ring or all elements if num
-    // is less than 1.
+    /**
+     * Print at most the last num elements from the ring or all elements if num
+     *is less than 1.
+     */
     public void printRing (int num) {
         if (num > this.ring.length || num < 1) {
             printRing ();
